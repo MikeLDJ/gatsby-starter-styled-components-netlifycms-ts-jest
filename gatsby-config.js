3e838dd20f6ec28d43d4254cf160df4c47dd73c4
MikeLD <mikeu76@gmail.com>
2020-01-04 23:42:11 +0100

    add typescript, add configuration, fix ts errors and warnings, add husky pre commit hook

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,14 @@
+/* eslint-disable @typescript-eslint/camelcase */
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Kick off your next, great Gatsby project with this default starter.
+    This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-typescript',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -31,4 +34,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
